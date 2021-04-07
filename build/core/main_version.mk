303ac8b9f886a5895755f8a56ff103e5bc09e6cc
KostyaJRZ <jrzroyale@gmail.com>
2021-04-07 22:22:42 +0300

    vendor/aosp: Bump version to 3.4 | Onyx

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -21,8 +21,8 @@ endif
 CUSTOM_BUILD_TYPE ?= UNOFFICIAL
 
 # PixelPlusUI Props
-PPUI_BASE_VERSION = 3.3
-PPUI_CODENAME := Tanzanite
+PPUI_BASE_VERSION = 3.4
+PPUI_CODENAME := Onyx
 
 CUSTOM_DATE_YEAR := $(shell date -u +%Y)
 CUSTOM_DATE_MONTH := $(shell date -u +%m)
